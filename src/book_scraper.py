48f4e7e8090e019f58739186fecf0791402b45e1
fzr <fzr@outlook.com>
2020-12-07 17:52:38 -0500

    Storing data in csv files

--- src/book_scraper.py
+++ src/book_scraper.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 reponse = requests.get('http://books.toscrape.com/catalogue/soumission_998/index.html')
@@ -8,18 +9,28 @@ bs = BeautifulSoup(html, 'html.parser')
 productData = {}
 
 productData['url'] = reponse.url
-productData['title'] = bs.find('div', class_='product_main').h1.get_text()
+productData['Title'] = bs.find('div', class_='product_main').h1.get_text()
 imgTag = bs.find('div', class_='carousel-inner').img
-productData['image url'] = imgTag.attrs['src']
-productData['image url'] = 'http://books.toscrape.com' + productData['image url'][5:]
-productData['product description'] = pTag = bs.find('article', class_='product_page').find('p', recursive=False).get_text()
+productData['Image url'] = imgTag['src']
+productData['Image url'] = 'http://books.toscrape.com' + productData['Image url'][5:]
+productData['Product description'] = pTag = bs.find('article', class_='product_page').find('p', recursive=False).get_text()
 category = bs.find('ul', class_='breadcrumb').select('li:nth-of-type(3)')[0].a
-productData['category'] = category.get_text()
+productData['Category'] = category.get_text()
 tableRows = bs.find('table', class_='table-striped').find_all('tr')
 
 
 for row in tableRows:
   productData[row.th.get_text()] = row.td.get_text()
 
-for key, value in productData.items():
-  print(f'{key}:\t{value}')
+del productData['Tax']
+del productData['Product Type']
+
+filePath = './data/' + productData['Category'] + '.csv'
+headers = productData.keys()
+items = productData.values()
+with open(filePath, 'w+') as csvFile:
+  writer = csv.writer(csvFile)
+  writer.writerow(headers)
+  writer.writerow(items)
+# for key, value in productData.items():
+#   print(f'{key}:\t{value}')
