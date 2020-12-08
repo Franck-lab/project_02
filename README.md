d864e3cd3160a33e36b807d7dc2f4fe739480d74
fzr <fzr@outlook.com>
2020-12-08 02:24:23 -0500

    Refactoring and data persistance to file

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-### Price Extraction with Python
+### Data Extraction with Python for Market Analysis
+
 Create and activate a virtual environment  
 `$ python3 -m venv <env_name>`  
 `$ source <env_name>/bin/activate`
@@ -7,4 +8,4 @@ Install the requirements
 `(env_name)$ pip install -r requirements.txt`
 
 Run the script  
-`(env_name)$ python book_scraper.py [category] ...`
+`(env_name)$ python book_scraper.py`
--- src/book_scraper.py
+++ src/book_scraper.py
@@ -1,6 +1,56 @@
 import requests
+import category_crawler as cc
+import sys
 import csv
 from bs4 import BeautifulSoup
+
+website = 'http://books.toscrape.com/
+productUrls = []
+
+def crawlPages(url):
+  try:
+    r = requests.get(url)
+  except requests.exceptions.RequestException as e:
+    print(e)
+    return False
+  bs = BeautifulSoup(r.content, 'html.parser')
+  return bs
+
+def getCategoryLinks():
+  categoryLinks = []
+  for category in bs.select('ul.nav-list ul li a'):
+    categoryLinks.append(category['href'])
+  return categoryLinks
+
+def browseCategories(baseUrl, pageLink):
+  bs = crawlPages(f'{baseUrl}{pageLink}')
+  getProductUrls(bs)
+  try:
+    nextPage = bs.find('li', class_='next').a['href']
+  except AttributeError:
+    pass
+  else:
+    browseCategory(baseUrl, nextPage)
+
+def getProductUrls(bs):
+  products = bs.find_all('article', class_='product_pod')
+  for article in products:
+    productLink = article.h3.a['href']
+    productUrls.append(f'http://books.toscrape.com/catalogue/{productLink[9:]}')
+
+bs = crawlPages(f'{website}index.html')
+categoryLinks = getCategoryLinks()
+
+for link in categoryLinks:
+  browseCategories(link.replace('index.html', ''), 'index.html')
+
+for link in productUrls:
+  bs = crawlPages(link)
+  data_dict = extractProductData(bs)
+  saveDataToFile(data_dict)
+
+
+while cc.checkInEntries(categories):
 
 reponse = requests.get('http://books.toscrape.com/catalogue/soumission_998/index.html')
 html = reponse.content
@@ -25,7 +75,7 @@ for row in tableRows:
 del productData['Tax']
 del productData['Product Type']
 
-filePath = './data/' + productData['Category'] + '.csv'
+filePath = f"./data/{productData['Category']}.csv"
 headers = productData.keys()
 items = productData.values()
 with open(filePath, 'w+') as csvFile:
